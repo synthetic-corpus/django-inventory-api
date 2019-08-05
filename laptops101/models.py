cb0d68e9031bb34a4bceaebec7bafbb469d8a924
unknown <joel@joelgonzaga.com>
2019-08-04 17:09:05 -0700

    Redesigned Models.

--- laptops101/models.py
+++ laptops101/models.py
@@ -13,24 +13,31 @@ class Manufacturer(models.Model):
     MANUFACTURER = models.CharField(max_length=255) # e.g. "Apple" or "Dell"
     WEBSITE = models.CharField(max_length=255)
 
-class GenericItem(models.Model):
-    """ All Items in inventory will have an asset tag, SKU, and Serial number.
-        SKU Uniquely identifies the type of item from manufacturer.
-        Serial is the manufactuer's serial.
-        Asset tag is internal to the company using this database. """
-    
+class ItemArchetype(models.Model):
+    """ Static Details about an asset that will not change from item to item 
+        e.g. All Dell XPS 4300 will ahve the Same SKU and Manufacturer, but 
+        different Serials, Asset Tags, users etc.
+    """
     SKU = models.CharField(max_length=255)
-    SERIAL = models.CharField(max_length=255)
-    ASSET = models.CharField(max_length=30)
-    MANUFACTURER = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+    COMMON_NAME = models.CharField(max_length=255) # e.g. "Dell XPS"
+    MANUFACTURER = models.ForeignKey("Manufacturer", on_delete=models.CASCADE)
+    NOTES = models.CharField(max_length=255)
+
+class TagInformation(models.Model):
+    """ Information common to all Assets, but will change on each asset """
+
     PURCHASE_DATE = models.DateField(_(""), auto_now=False, auto_now_add=False)
     PURCHASE_COST = models.DecimalField(max_digits=7, decimal_places=2)
-    ASSIGNED_USER = models.ForeignKey(EndUser, on_delete=models.SET_NULL)
+    ASSIGNED_USER = models.ForeignKey("EndUser", on_delete=models.SET_NULL)
+    ASSET_TAG = models.CharField(max_length=8)
 
 class Laptop(models.Model):
     """ Any Laptop Computer """
+
     CPU_MODEL = models.CharField(max_length=128) #e.g. Intel i7
     CPU_SPEED = models.DecimalField(max_digits=3, decimal_places=2) # Speed of CPU in GHZ
     RAM = models.IntegerField() # RAM in GB
     HDD = models.DecimalField(max_digits=4, decimal_places = 2) # HDD size in Terabytes
-    COMMON_ATTRIBUTES = models.ForeignKey(GenericItem, on_delete=models.CASCADE)
+    ARCHETYPE = models.ForeignKey("ItemArchetype", on_delete=models.CASCADE)
+    TAG = models.models.ForeignKey("TagInformation", on_delete=models.SET_NULL)
+    NOTES = models.CharField(max_length=255)
