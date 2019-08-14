2936757d4321e133a8cbc9d15b51a292123cc5a9
unknown <joel@joelgonzaga.com>
2019-08-13 17:50:19 -0700

    Set a successful create request with viewsets.

--- laptops101/views.py
+++ laptops101/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.settings import api_settings
 
 from laptops101.models import EndUser, Manufacturer
 from laptops101 import serializers as s
@@ -11,16 +12,29 @@ class EndUserViewSet(viewsets.ViewSet):
     """ CRUD requests for the end user Model. Sending generic replies only. """
     serializer_class = s.EndUserSerializer
 
+    def get_success_headers(self, data):
+        try:
+            return {'Location': str(data[api_settings.URL_FIELD_NAME])}
+        except (TypeError, KeyError):
+            return {}
+
     def list(self, request):
-        content = {'Message': 'Retrieved List'}
-        return Response(content)
+        queryset = EndUser.objects.all()
+        serializer = s.EndUserSerializer(queryset, many=True)
+        return Response(serializer.data)
     
     def create(self, request):
-        return Response({'Message': 'Retrieved List'})
+        serializer = s.EndUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
     
     def retrieve(self, request, pk=None):
-        """ Retrieves an object. Get crequest. """
-        return Response({'http_method': 'GET'})
+        queryset = EndUser.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = s.EndUserSerializer(user)
+        return Response(serializer.data)
     
     def update(self, request, pk=None):
         """ Handles updating an object. Put request. """
